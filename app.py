5113d6f1d395b96f82391c343a5f928270cc86d4
Вилков Виталий <takao.nakamuro@yandex.ru>
2025-04-01 10:04:24 +0300

    обновил gitignore

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
+
+@app.route('/category')
+def category():
+    return render_template('category.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
